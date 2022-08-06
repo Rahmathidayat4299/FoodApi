b8b3075bba045e5fde2f2929e4379ed23b1d0b2d
Rahmathidayat4299 <hrahmat950@gmail.com>
2022-08-07 02:57:55 +0700

    cleancode

--- app/src/main/java/com/rahmathidayat/post/MainVM.kt
+++ app/src/main/java/com/rahmathidayat/post/MainVM.kt
@@ -5,31 +5,13 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.rahmathidayat.seafood.Comments
 import com.rahmathidayat.seafood.Meal
 import com.rahmathidayat.seafood.ModelFood
-import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainVM(private val repository: Repository) : ViewModel() {
-    //    val dataPost: MutableLiveData<Response<Post>> = MutableLiveData()
-//    val dataFood: MutableLiveData<Response<List<ModelFood>>> = MutableLiveData()
-//    fun getPost() {
-//        viewModelScope.launch {
-//            val response = repository.getPost()
-//            dataPost.value = response
-//        }
-//    }
-//
-//    fun listFood(categori: String) {
-//        viewModelScope.launch {
-//            val response = repository.getListFood(categori)
-//            dataFood.value = response
-//        }
-//    }
     private val list = MutableLiveData<ArrayList<Meal>>()
 
 
